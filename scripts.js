d841bc3e1db0daaca235ccef78cb28ab5cc41dee
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2024-05-17 12:27:55 -0700

    Reorganize 16x16 squares into 16 rows of 16 square
    Previously used flexwrap on one container on ~300 squares.
    Flexbox considers each row-by-row separate, so needed to reorganize to 16 rows of 16 for equal sizing across rows.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,16 @@
 // javascript
 
-// create 16*16-1 copies of "square" class div
-const box = document.querySelector(".square");
+
+// create 16 copies of rows containing squares
 const container = document.querySelector(".container");
-for (let i =1; i<16*16;i++){
-    const boxToAdd = document.createElement("div");
-    boxToAdd.classList.add("square");
-    container.appendChild(boxToAdd);
+for (let i = 0; i<16; i++){
+    // create 1 "row" div containing 16 copies of "square" class div
+    const rowToAdd = document.createElement("div");
+    rowToAdd.classList.add("row");
+    for (let j =0; j<16;j++){
+        const boxToAdd = document.createElement("div");
+        boxToAdd.classList.add("square");
+        rowToAdd.appendChild(boxToAdd);
+    }
+    container.appendChild(rowToAdd);
 }
