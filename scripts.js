20a59259945a7524fa6471e5400dc096718ac609
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2024-05-17 11:50:26 -0700

    Create 16x16 div elements for grid
    Use loops and DOM manipulations to make many squares
    
    Assign square to "square" class.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
 // javascript
+
+// create 16*16-1 copies of "square" class div
+const box = document.querySelector(".square");
+const container = document.querySelector(".container");
+for (let i =1; i<16*16;i++){
+    const boxToAdd = document.createElement("div");
+    boxToAdd.classList.add("square");
+    container.appendChild(boxToAdd);
+}
