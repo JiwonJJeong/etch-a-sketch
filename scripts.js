b1c983e5f765970868ff1185a96f92b309da760d
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2024-05-17 14:00:32 -0700

    Reset grid upon pressing button

--- scripts.js
+++ scripts.js
@@ -2,23 +2,42 @@
 
 
 // create 16 copies of rows containing squares
-const container = document.querySelector(".container");
-for (let i = 0; i<16; i++){
-    // create 1 "row" div containing 16 copies of "square" class div
-    const rowToAdd = document.createElement("div");
-    rowToAdd.classList.add("row");
-    for (let j =0; j<16;j++){
-        // create 1 square in the row and add an event listener for hover
-        const boxToAdd = document.createElement("div");
-        boxToAdd.classList.add("square");
-        boxToAdd.addEventListener("mouseover", function(event) {
-            doHoverEffect(event);
-        });
-        rowToAdd.appendChild(boxToAdd);
+function generateGrid(){
+    const container = document.querySelector(".container");
+    for (let i = 0; i<16; i++){
+        // create 1 "row" div containing 16 copies of "square" class div
+        const rowToAdd = document.createElement("div");
+        rowToAdd.classList.add("row");
+        for (let j =0; j<16;j++){
+            // create 1 square in the row and add an event listener for hover
+            const boxToAdd = document.createElement("div");
+            boxToAdd.classList.add("square");
+            boxToAdd.addEventListener("mouseover", function(event) {
+                doHoverEffect(event);
+            });
+            rowToAdd.appendChild(boxToAdd);
+        }
+        container.appendChild(rowToAdd);
     }
-    container.appendChild(rowToAdd);
 }
 
 function doHoverEffect(event){
     event.target.style.backgroundColor = "red"
+}
+
+// initial grid generation
+generateGrid();
+
+// give button at top of screen ability to remove and generate new grid
+const resetButton = document.querySelector(".reset");
+resetButton.addEventListener("click", function(){
+    deleteGrid();
+    generateGrid();
+});
+
+function deleteGrid(){
+    const container = document.querySelector(".container");
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
 }
